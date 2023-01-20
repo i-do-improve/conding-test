e67072111f1fee18000be473117ad677be94b9fd
sobaman <sobaman522@gmail.com>
2023-01-20 22:05:05 +0900

    이것은 평균구하기 문제

---
+++ soba/jan/2022-01-20-평균구하기문제.java
@@ -0,0 +1,10 @@
+public double solution(int[] numbers) {
+        double answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+        answer = (answer / numbers.length);
+        return answer;
+        // -> return answer / numbers.length; 리턴에 바로 넣어주면 더 깔끔하다!
+    }
+}
