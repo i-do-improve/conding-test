86668200d49e0eb1c4e543e3f2cb1eff0eeb13e9
sobaman <sobaman522@gmail.com>
2023-01-20 22:14:29 +0900

    이것은 합의 평균 구하기 문제 찐최종버전

---
+++ soba/jan/2022-01-20-합의평균구하기문제.java
@@ -0,0 +1,9 @@
+public double solution(int[] numbers) {
+    double answer = 0;
+    for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];}
+    answer = (answer / numbers.length);
+    return answer;
+        // -> return answer / number.length; : return에 바로 넣어주면 더 깔끔하다!
+    }
+}
