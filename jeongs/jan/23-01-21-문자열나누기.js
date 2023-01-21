5d35807f775f12099d4d8b83fa5693463c185ece
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-01-21 12:48:40 +0900

    feat: 코드 단순화

--- jeongs/jan/23-01-21-문자열나누기.js
+++ jeongs/jan/23-01-21-문자열나누기.js
@@ -6,20 +6,12 @@ function solution(s) {
     
     array.forEach((str, idx) => {
         if (counts[0] === counts[1] && counts[0] > 0 && counts[1] > 0) {
-        // counts[0] === counts[1] && 각각이 1이상일 때,
             result+= 1;
             target = str;
             counts = [0, 0];
         }
-        if (str === target) {
-        // 현재 값이 타겟일 때,
-            counts[0]+= 1;
-            return;
-        }
-        // 현재 값이 타겟이 아닐 때,
-        if (str !== target) {
-            counts[1]+= 1;
-        }
+
+        (str === target) ? counts[0] += 1 : counts[1] += 1;
     })
     return result + 1;
 }
