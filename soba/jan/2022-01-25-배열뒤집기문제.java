1b1f652d915825ae0b5b029b1e589bd5a83472b4
sobaman <sobaman522@gmail.com>
2023-01-25 22:54:58 +0900

    이것은 배열뒤집기문제!

--- soba/jan/2022-01-25-배열뒤집기문제.java
+++ soba/jan/2022-01-25-배열뒤집기문제.java
@@ -1,9 +1,9 @@
 class Solution {
-        public int[] solution(String[] strlist) {
-            int [] answer = new int [strlist.length];
-            for(int i = 0; i < strlist.length; i++){
-                answer[i] = strlist[i].length();
-            }
-            return answer;
+    public int[] solution(int[] num_list) {
+        int[] Arr = new int [num_list.length];
+        for(int i = 0; i < num_list.length; i++){
+            Arr[i] = num_list[(num_list.length - 1) - i];
         }
-}
+        return Arr;
+    }
+}}
