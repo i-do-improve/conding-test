985d3cf093a6250ce4f4e086c750e9bcfce24d60
sobaman <sobaman522@gmail.com>
2023-02-20 00:50:50 +0900

    [2023-02-19] n의배수고르기 문제입니다.

---
+++ soba/feb/2023-02-19-n의배수고르기.java
@@ -0,0 +1,23 @@
+class n의배수고르기 {
+    public int[] solution(int n, int[] numlist) {
+        int length = 0;
+        
+        for(int i = 0; i < numlist.length; i++){
+            if(numlist[i] % n == 0){
+                length++;
+            }
+        }
+        
+        int[] answer = new int[length];
+        
+        int j = 0;
+        
+        for(int i = 0; i < numlist.length; i++){
+            if(numlist[i] % n == 0){
+                answer[j] = numlist[i];
+                j++;
+            }
+        }
+        return answer;
+    }
+}
