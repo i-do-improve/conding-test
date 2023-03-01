a97aeed28f52040d78dcc233abd5a47921d0e707
sobaman <sobaman522@gmail.com>
2023-03-01 22:15:12 +0900

    [2023-02-278] 가장큰수찾기 문제입니다....

---
+++ soba/feb/2023-02-28-가장큰수찾기.java
@@ -0,0 +1,16 @@
+package feb;
+
+class 가장큰수찾기 {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        for(int i = 0; i < array.length; i++){
+            if(answer[0] <= array[i]){
+                answer[0] = array[i] ;
+                answer[1] = i ;
+            }
+        }
+
+        return answer;
+    }
+}
