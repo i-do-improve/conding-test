994aca291b0515d162d6bb79736764bddbce9113
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-03-11 00:27:31 +0900

    feat: 코드 간소화

--- jeongs/mar/23-03-11-세균증식.js
+++ jeongs/mar/23-03-11-세균증식.js
@@ -1,10 +1,6 @@
 function solution(n, t) {
     const DFS = (L, sum) => {
-        if (L === t) {
-            return sum * 2;   
-        }
-        
-        return DFS(L + 1, sum * 2);
+        return (L === t) ? sum * 2 : DFS(L + 1, sum * 2);
     }
     
     return DFS(1, n);
