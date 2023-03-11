9b064a5131f8e88f4d542334deb978042996cf74
sobaman <sobaman522@gmail.com>
2023-03-12 01:26:03 +0900

    [2023-03-11] Object -> Objects로 변경

--- soba/mar/2023-03-11-서울에서김서방찾기.java
+++ soba/mar/2023-03-11-서울에서김서방찾기.java
@@ -9,7 +9,7 @@ class 서울에서김서방찾기 {
                 name += i;
                 // == 는 주소값을 비교하는 연산자이고, equals()는 주소 안의 데이터 값을 비교하는 메서드
                 // break; 하면 찾은 즉시 반환하여 효율적
-                // Object.equals()를 사용하면 Null을 체크하기 때문에 NullPointerException이 발생하지 않음
+                // Objects.equals()를 사용하면 Null을 체크하기 때문에 NullPointerException이 발생하지 않음
             }
         }
         String answer = "김서방은 " + name +"에 있다";
