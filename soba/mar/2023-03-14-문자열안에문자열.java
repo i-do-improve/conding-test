c4e6ef361566bca2a4fb584823e7410747d615a2
sobaman <sobaman522@gmail.com>
2023-03-14 23:51:32 +0900

    [2023-03-14] 문자열안에 문자열 문제입니다.

---
+++ soba/mar/2023-03-14-문자열안에문자열.java
@@ -0,0 +1,13 @@
+package mar;
+
+public class 문자열안에문자열 {
+
+    class Solution {
+        public int solution(String str1, String str2) {
+            int answer = 0;
+
+            return answer = str1.contains(str2) ? 1 : 2;
+        }
+    }
+
+}
