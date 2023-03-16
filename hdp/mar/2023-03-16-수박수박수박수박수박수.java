2ce98f71f1924f1460950413ad3dd01ba81a370e
DP <tree1787@naver.com>
2023-03-17 03:48:38 +0900

    2023-03-16-수박수박수박수박수박수.java

---
+++ hdp/mar/2023-03-16-수박수박수박수박수박수.java
@@ -0,0 +1,13 @@
+class Solution {
+	public String solution(int n) {
+		StringBuilder sb = new StringBuilder();
+		for(int i=1;i<=n;i++){
+			if(i%2==1){
+				sb.append("수");
+			}else{
+				sb.append("박");
+			}
+		}
+		return sb.toString();
+	}
+}
