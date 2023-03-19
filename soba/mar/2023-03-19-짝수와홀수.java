64f69d90deb05785d886e4344bd09665f8b3d84d
sobaman <sobaman522@gmail.com>
2023-03-19 21:46:53 +0900

    [2023-03-19] 짝수와 홀수 문제입니다.

---
+++ soba/mar/2023-03-19-짝수와홀수.java
@@ -0,0 +1,13 @@
+package mar;
+
+class 짝수와홀수 {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
