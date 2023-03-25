7acf4fe940848b9c47183a090ed50fc7b84d6088
sobaman <sobaman522@gmail.com>
2023-03-25 16:04:54 +0900

    [2023-03-24] 7의 개수 문제입니다.

--- soba/mar/2023-03-24-칠의개수.java
+++ soba/mar/2023-03-24-칠의개수.java
@@ -3,6 +3,7 @@ package mar;
 // 하나하나 빼야해 그럴려면? string형으로 바꿔야지
 // + ""; 를 사용하면 쉽게 int -> string으로 바꿀수잇다는 것을 배운적이잇다
 
+// update 2023-03-25 커밋 수정함
 
 class 칠의개수 {
     public int solution(int[] array) {
