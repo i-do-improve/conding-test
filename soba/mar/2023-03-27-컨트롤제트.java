4c5470d80233958d20919f9f448091bfa90c4032
sobaman <sobaman522@gmail.com>
2023-03-27 20:58:21 +0900

    [2023-03-27] 컨트롤 제트 문제입니다.

---
+++ soba/mar/2023-03-27-컨트롤제트.java
@@ -0,0 +1,18 @@
+package mar;
+
+class 컨트롤제트 {
+    public int solution(String s) {
+        int answer = 0;
+        
+        String[] str = s.split(" ");
+        
+        for(int i = 0 ; i < str.length; i++){
+            if(str[i].equals("Z")){
+                answer -= Integer.parseInt(str[i-1]);
+            }else{
+                answer += Integer.parseInt(str[i]);
+            }
+        }
+        return answer;
+    }
+}
