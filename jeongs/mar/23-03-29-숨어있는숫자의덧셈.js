399701c9a74d15f84e0d48bf1d2c8dc23de7ab01
Jiyoung Kim <kimm.jeongs@gmail.com>
2023-04-23 20:21:59 +0900

    feat: 수정

--- jeongs/mar/23-03-29-숨어있는숫자의덧셈.js
+++ jeongs/mar/23-03-29-숨어있는숫자의덧셈.js
@@ -1,6 +1,8 @@
 // 12:08 ~ 12:09
 function solution(my_string) {
-    return my_string.split('').reduce((acc, word) => {
+    return my_string
+        .split('')
+        .reduce((acc, word) => {
         acc += (Number(word)) ? Number(word) : 0;
         return acc;
     }, 0);
